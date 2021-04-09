21ac7b0d5966d4349289c736951a122e574518b0
zw-95 <35022819+zw-95@users.noreply.github.com>
2021-04-09 11:25:16 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -10,7 +10,7 @@ export const GitHubShareButton: React.FC = () => {
       rel='noopener noreferrer'
       className={styles.githubCorner}
       aria-label='View source on GitHub'
-      style='display=none'
+      style='display:none'
     >
       <svg
         width='80'
